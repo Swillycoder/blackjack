c8ed4f8ae125b6a86bce74833e7c7fe12d1791ef
Edward 'Ted <159036536+Swillycoder@users.noreply.github.com>
2025-05-25 02:34:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -242,7 +242,7 @@ function stick() {
 
     // Start revealing dealer cards with delay
     function revealNextCard() {
-        if ((dealerScore >= 17 && dealerScore >= playerScore) || deck.length === 0) {
+        if (dealerScore >= 17 || dealerScore >= playerScore) {
             clearInterval(dealerInterval);
 
             // Delay result calculation
